52a1b2c420e1b1bf7caf7ae9d4d6d4f65a4ae24e
Ss <solutionguru321@gmail.com>
2026-07-25 16:09:27 +0530

    chore: remove unused classes (invoice, pricing strategy, legacy Order, order-line repository)

--- src/main/java/com/order/service/service/Invoice.java
+++
@@ -1,7 +0,0 @@
-package com.order.service.service;
-
-import com.order.service.entity.OrderEntity;
-
-public interface Invoice {
-    String generateInvoice(OrderEntity order);
-}
--- src/main/java/com/order/service/service/PricingStrategy.java
+++
@@ -1,9 +0,0 @@
-package com.order.service.service;
-
-import com.order.service.dto.OrderItem;
-
-import java.util.List;
-
-public interface PricingStrategy {
-    double calculateTotal(List<OrderItem> items);
-}
